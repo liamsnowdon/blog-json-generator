8443a438db659557ed8898654e64cd93b84031ae
Liam Snowdon <snowdon.liam@gmail.com>
2020-12-09 06:14:36 +0000

    Migrate post creator

---
+++ assets/js/data.js
@@ -0,0 +1,28 @@
+var categories = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'General'
+];
+
+var tags = [
+  'Responsive',
+  'Accessibility',
+  'Forms',
+  'Documentation',
+  'Architecture',
+  'Sass',
+  'Typography'
+];
+
+var posts = [
+  'Using the &lt;picture&gt; element for responsive images',
+  'Handling button hover states for touchscreen devices',
+  'Integrating Gulp 4 into your workflow',
+  'Pre-compiling Nunjucks templates with Gulp'
+];
+
+window.BlogGenerator = window.BlogGenerator || {};
+window.BlogGenerator.Categories = categories;
+window.BlogGenerator.Tags = tags;
+window.BlogGenerator.Posts = posts;
---
+++ gulpfile.babel.js
@@ -0,0 +1,41 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import cssnano from 'cssnano';
+import mqPacker from 'css-mqpacker';
+import autoprefixer from 'autoprefixer';
+import browserSync from 'browser-sync';
+
+browserSync.create();
+
+sass.compiler = require('dart-sass');
+
+const postcssPlugins = [
+    autoprefixer({ cascade: false }),
+    mqPacker(),
+    cssnano()
+];
+
+export const css = () => {
+    return gulp.src('./assets/scss/styles.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(postcssPlugins))
+        .pipe(gulp.dest('./assets/css'));
+}
+
+export const watchScss = () => {
+    gulp.watch('./assets/scss/**/*.scss', css);
+}
+
+export const sync = () => {
+    browserSync.init({
+        server: {
+            baseDir: './',
+            index: "index.html"
+        }
+    });
+}
+
+export const watch = gulp.series(css, watchScss);
+
+export default css;
